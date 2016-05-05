493d80a0bf4636629550033dfca0413e4ac0218c
MELISSA GUION <M.GUION@GMAIL.COM>
2016-05-05 08:40:53 -0500

    added Google font Share Tech Mono to application.scss

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,7 +9,7 @@ class Product < ActiveRecord::Base
 
   has_attached_file :avatar, :styles => {
     :medium => "400x400#",
-    :thumb => "150x150>"
+    :thumb => "100x100>"
     },
     :default_url => "missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
