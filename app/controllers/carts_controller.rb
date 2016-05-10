8fb88325b72a18dd854ff74487def60aa0409a43
MELISSA GUION <M.GUION@GMAIL.COM>
2016-05-10 10:11:32 -0500

    moves cart destroy from model to controller and includes currentcart in cart controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,21 @@
 class CartsController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
+
   def edit
   end
+
+
+    def destroy
+    if @cart.destroy
+      session[:cart_id] = nil
+      flash[:notice] = "Cart was successfully deleted."
+      redirect_to shop_path
+    else
+      flash.now[:alert] = "Could not delete cart."
+      redirect_to edit_cart_path
+    end
+  end
+
+
 end
